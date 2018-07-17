52ceea5cd401bce419672babb2b60d39e161f80e
AnnabelleCat <pikainka@gmail.com>
2018-07-17 21:10:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,3 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 end
+
+get '/' do
+  "Hello, World"
+end
+
+get '/name' do
+  "My name is Anika"
+end
+
+get'/hometown' do
+  "Allen"
+end
+
+get '/favorite-song'
+  "french folk song"
+end
